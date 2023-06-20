12f8d68b8d17627952b7b3f8796601ec9e343520
Max Paterson-Jones <100119585+Wabzab@users.noreply.github.com>
2023-06-20 22:58:46 +0200

    Typos
    Few typos found around the codebase.

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -1,8 +1,9 @@
 extends Character
 
 @export var inventory_data: InventoryData
-@onready var interract_area: Area2D = $InterractArea
-@onready var interract_label: Label = $UI/Interactions/InterractLabel
+@onready var interact_area: Area2D = $InteractArea
+@onready var interact_label: Label = $UI/Interactions/InteractLabel
+@onready var interact_hover: TextureRect = $UI/Interactions/InteractHover
 
 signal toggle_inventory
 
@@ -14,19 +15,22 @@ func _unhandled_input(event: InputEvent) -> void:
 		toggle_inventory.emit()
 	
 	if Input.is_action_just_pressed("interract"):
-		if interract_area.has_overlapping_bodies():
-			var interactable = interract_area.get_overlapping_bodies()[0]
+		if interact_area.has_overlapping_bodies():
+			var interactable = interact_area.get_overlapping_bodies()[0]
 			if global_position.distance_to(interactable.global_position) <= reach:
 				interactable.player_interact()
 
 func _physics_process(delta) -> void:
 	# Update interract area
-	interract_area.global_position = get_global_mouse_position()
-	interract_label.hide()
-	if interract_area.has_overlapping_bodies():
-		interract_label.global_position = get_viewport().get_mouse_position() + Vector2(0, 10)
-		interract_label.show()
-		interract_label.text = interract_area.get_overlapping_bodies()[0].name
+	interact_area.global_position = get_global_mouse_position()
+	interact_label.hide()
+	if interact_area.has_overlapping_bodies():
+		var interactable = interact_area.get_overlapping_bodies()[0]
+		interact_label.text = interactable.name
+		# Center label below cursor
+		interact_label.global_position = get_viewport().get_mouse_position()
+		interact_label.global_position += Vector2(-interact_label.get_rect().size.x/2, 10)
+		interact_label.show()
 	
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
