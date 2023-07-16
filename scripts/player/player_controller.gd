ace83e2f17ec1f347343c42892ec3d2b827a85e7
Max Paterson-Jones <100119585+Wabzab@users.noreply.github.com>
2023-07-16 09:39:22 +0200

    Rework weapon equipment
    Small changes to how weapons are equipped on the character, with weapons able to go from hand to holster easily.

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -15,18 +15,22 @@ const THROW_FORCE: int = 10
 @onready var interact_label: Label = $UI/Interactions/InteractLabel
 @onready var interact_hover: TextureRect = $UI/Interactions/InteractHover
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
-@onready var holster_a = $Back/HolsterA
-@onready var holster_b = $Back/HolsterB
+@onready var holster_a: Node2D = $Back/HolsterA
+@onready var holster_b: Node2D = $Back/HolsterB
 
 signal toggle_inventory
 signal swap_weapon(index: int)
 
-var armours: Array[ArmourData] = []
-var weapons: Array[WeaponData] = [null, null, null]
+var worn_armours: Array[ArmourData] = []
+var worn_weapons: Array[WeaponData] = [null, null, null]
+var holsters: Array[Node2D]
 var held_index = 0
+var old_index = 0
+var holstered = false
 var speed = get_stat("speed") * get_stat("speed_mult")
 
 func init_setup() -> void:
+	holsters = [holster_a, holster_b]
 	equipment_data.inventory_updated.connect(on_equipment_updated)
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -43,18 +47,31 @@ func _unhandled_input(event: InputEvent) -> void:
 				interactable.player_interact()
 	
 	if Input.is_action_just_pressed("primary"):
-		if weapons[held_index]:
-			weapons[held_index].primary()
+		if worn_weapons[held_index]:
+			worn_weapons[held_index].primary()
+		else:
+			print("Punch!")
 	
 	if Input.is_action_just_pressed("secondary"):
-		if weapons[held_index]:
-			weapons[held_index].secondary()
+		if worn_weapons[held_index]:
+			worn_weapons[held_index].secondary()
+		else:
+			print("Super Punch!")
 	
 	if Input.is_action_just_pressed("scroll_up"):
-		swap_held_weapon(-1)
+		swap_held_weapon(held_index-1)
 	
 	if Input.is_action_just_pressed("scroll_down"):
-		swap_held_weapon(1)
+		swap_held_weapon(held_index+1)
+	
+	if Input.is_action_just_pressed("holster"):
+		holstered = not holstered
+		if holstered:
+			#holster_weapon(old_index)
+			old_index = held_index
+			swap_held_weapon(2)
+		else:
+			swap_held_weapon(old_index)
 
 func _physics_process(delta) -> void:
 	# Update interract area
@@ -98,15 +115,15 @@ func on_equipment_updated(equipment_data: InventoryData) -> void:
 	print("||------------------------------||")
 	for stat in stats:
 		print("%s: %s" % [stat, stats[stat]])
-	print("Weapon A: %s" % weapons[0].name if weapons[0] else "Weapon A: ...")
-	print("Weapon B: %s" % weapons[1].name if weapons[1] else "Weapon B: ...")
+	print("Weapon A: %s" % worn_weapons[0].name if worn_weapons[0] else "Weapon A: ...")
+	print("Weapon B: %s" % worn_weapons[1].name if worn_weapons[1] else "Weapon B: ...")
 
-func equip_armours(equipment: Array[SlotData]) -> void:
-	for item in equipment:
-		if item:
-			equip(item.item_data)
+func equip_armours(armours: Array[SlotData]) -> void:
+	for armour in armours:
+		if armour:
+			equip_armour(armour.item_data)
 
-func equip(armour: ArmourData) -> void:
+func equip_armour(armour: ArmourData) -> void:
 	mod_stat("defence", armour.defence)
 	for sm in armour.stat_modifiers:
 		mod_stat(sm.get_stat(), sm.get_value())
@@ -115,23 +132,21 @@ func equip(armour: ArmourData) -> void:
 	for rm in armour.resist_modifiers:
 		mod_damage_resist(rm.get_stat(), rm.get_value())
 
-func equip_weapons(equipment: Array[SlotData]) -> void:
-	weapons[0] = null
-	for child in holster_a.get_children():
-		holster_a.remove_child(child)
-	if equipment[0]:
-		weapons[0] = equipment[0].item_data
-		holster_a.add_child(Globals.create_weapon(weapons[0]))
-	weapons[1] = null
-	for child in holster_b.get_children():
-		holster_b.remove_child(child)
-	if equipment[1]:
-		weapons[1] = equipment[1].item_data
-		holster_b.add_child(Globals.create_weapon(weapons[1]))
+func equip_weapons(weapons: Array[SlotData]) -> void:
+	for i in weapons.size():
+		equip_weapon(i, holsters[i], weapons[i])
 
-func swap_held_weapon(offset: int) -> void:
-	held_index = held_index+offset
+func equip_weapon(index: int, holster: Node2D, weapon: SlotData) -> void:
+	worn_weapons[index] = null
+	for child in holster.get_children():
+		holster.remove_child(child)
+	if weapon:
+		worn_weapons[index] = weapon.item_data
+		holster.add_child(Globals.create_weapon(weapon.item_data))
+
+func swap_held_weapon(index: int) -> void:
+	held_index = index
 	if held_index < 0:
-		held_index = 2
-	held_index = held_index%3
+		held_index = 1
+	held_index = held_index%2
 	swap_weapon.emit(held_index)
