8c90bc54f6721e0e247dc23b3bf96e65db3462df
Max Paterson-Jones <100119585+Wabzab@users.noreply.github.com>
2023-08-12 00:30:07 +0200

    Tweaked status effects
    Tweaked some base behaviours of effects and made the on fire sample effect more demonstrative of how to setup a good damage over time effect.

--- components/hurtbox/hurtbox_component.gd
+++ components/hurtbox/hurtbox_component.gd
@@ -12,20 +12,22 @@ signal entity_damaged(hitbox: HitboxComponent, damage_datas: Array[DamageData])
 ## Number of damage instances per second per entity
 @export var damage_rate: float = 0
 
-var hurt_entities: Array[HurtEntity] = []
+var hurt_entities: Array[TimedVariant] = []
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is HitboxComponent:
-		if hurt_entities.filter(func(entity: HurtEntity): return entity.area == area).is_empty() \
+		if hurt_entities.filter(func(entity: TimedVariant): return entity.variant == area).is_empty() \
 				and (area.faction != faction or area.faction == Globals.FACTIONS.NONE):
 			area.handle_hurbox_collision(self)
-			var hurt_entity = HurtEntity.new()
-			hurt_entity.set_data(area, damage_rate)
-			hurt_entity.connect("timeout", remove_entity)
+			var hurt_entity = TimedVariant.new()
+			add_child(hurt_entity)
+			hurt_entity.start(area, 1/damage_rate)
+			hurt_entity.timeout.connect(remove_entity)
 			hurt_entities.append(hurt_entity)
 			entity_damaged.emit(area, damage_datas)
 
-func remove_entity(hurt_entity: HurtEntity) -> void:
+func remove_entity(hurt_entity: TimedVariant) -> void:
+	remove_child(hurt_entity)
 	hurt_entities.remove_at(hurt_entities.find(hurt_entity))
-	if hurt_entity.area in get_overlapping_areas():
-		_on_area_entered(hurt_entity.area)
+	if hurt_entity.variant in get_overlapping_areas():
+		_on_area_entered(hurt_entity.variant)
