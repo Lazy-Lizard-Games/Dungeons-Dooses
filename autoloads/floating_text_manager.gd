46bc1e625a60c173b536f1b88cec0ac984922ac3
Max Paterson-Jones <100119585+Wabzab@users.noreply.github.com>
2023-08-12 12:06:27 +0200

    Display effect on player
    Setup UI to display the players weapon skills, with alternate styles for when they are on cooldown, and a display for the status effects currently on a character, with a label to display the stack size.

--- autoloads/floating_text_manager.gd
+++ autoloads/floating_text_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @onready var root = get_tree().root
-@onready var floating_damage = preload("res://scenes/ui/floating_damage.tscn")
+@onready var floating_damage = preload("res://scenes/ui/floating_damage/floating_damage.tscn")
 
 func create_damage_float(position: Vector2, damage_data: DamageData) -> DamageFloat:
 	var damage_float = floating_damage.instantiate()
