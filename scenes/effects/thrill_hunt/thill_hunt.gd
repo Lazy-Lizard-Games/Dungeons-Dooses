8ab531e7f10d78d66b8fb73da2df28e3fd0e1f13
Max Paterson-Jones <100119585+Wabzab@users.noreply.github.com>
2023-09-14 14:47:40 +0200

    Second Berserker Skill
    Nearly exact copy of the previous skill except this one gives movement speed per stack of blood.

---
+++ scenes/effects/thrill_hunt/thill_hunt.gd
@@ -0,0 +1,42 @@
+extends Effect
+
+# Give increased movement speed to the player based
+# on the number of blood stacks that the player has.
+
+@export var move_speed_per_stack: float = 0.01
+@export var blood_instance: EffectInstance
+var blood_effect: Effect
+
+
+func _process(delta):
+	if blood_effect:
+		return
+	blood_effect = container.get_effect(blood_instance)
+	if blood_effect:
+		on_blood_stack_changed(blood_effect.stacks)
+		blood_effect.stack_changed.connect(on_blood_stack_changed)
+		blood_effect.exited_tree.connect(on_blood_stack_exited)
+
+
+func _add_stack() -> void:
+	if stacks < effect_instance.max_stacks:
+		stacks += 1
+	stack_changed.emit(1)
+	if blood_effect:
+		container.stats_component.move_speed_mult -= blood_effect.stacks * (stacks-1) * move_speed_per_stack
+		container.stats_component.accelerate_mult -= blood_effect.stacks * (stacks-1) * move_speed_per_stack
+		on_blood_stack_changed(blood_effect.stacks)
+
+
+func _get_description() -> String:
+	var effect = move_speed_per_stack*stacks
+	var desc = "[b]Movement Speed[/b]: +%s" % effect
+	return desc+"%"
+
+func on_blood_stack_changed(change: int) -> void:
+	container.stats_component.move_speed_mult += change * stacks * move_speed_per_stack
+	container.stats_component.accelerate_mult += change * stacks * move_speed_per_stack
+
+
+func on_blood_stack_exited(effect: Effect) -> void:
+	blood_effect = null
