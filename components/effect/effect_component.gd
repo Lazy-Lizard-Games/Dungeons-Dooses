b6d1e2a2192b8e22ed07a1ea8e22f66e90de6143
Wabzab <patersonjones.max@gmail.com>
2024-04-07 09:55:30 +0200

    fix: bugs with new effects system

--- components/effect/effect_component.gd
+++ components/effect/effect_component.gd
@@ -41,10 +41,13 @@ func add_passive_effect(effect: PassiveEffect) -> void:
 
 ## Removes any passive effects matching the unique identifier.
 func remove_passive_effect(uid: StringName) -> void:
+	var passive_effects_copy = passive_effects.duplicate()
 	for effect in passive_effects:
 		if effect.uid == uid:
 			passive_effect_removed.emit(effect)
+			passive_effects_copy.erase(effect)
 			effect.queue_free()
+	passive_effects = passive_effects_copy
 
 # ----------------------------------------------------------------- #
 
@@ -58,14 +61,18 @@ func add_active_effect(effect: ActiveEffect) -> void:
 	else:
 		active_effect_added.emit(effect)
 		effect.expired.connect(on_effect_expired.bind(effect), CONNECT_ONE_SHOT)
+		active_effects.append(effect)
 		add_child(effect)
 
 ## Removes any active effects matching the unique identifier.
 func remove_active_effect(uid: StringName) -> void:
+	var active_effects_copy = active_effects.duplicate()
 	for effect in active_effects:
 		if effect.uid == uid:
 			active_effect_removed.emit(effect)
+			active_effects_copy.erase(effect)
 			effect.queue_free()
+	active_effects = active_effects_copy
 
 # ----------------------------------------------------------------- #
 
@@ -74,16 +81,24 @@ func remove_active_effect(uid: StringName) -> void:
 ## Adds an active effect to be managed by the component.
 func add_damage_effect(effect: DamageEffect) -> void:
 	damage_effect_added.emit(effect)
+	damage_effects.append(effect)
 	add_child(effect)
 
 ## Removes any damage effects matching the unique identifier.
 func remove_damage_effect(uid: StringName) -> void:
+	var damage_effects_copy = damage_effects.duplicate()
 	for effect in damage_effects:
 		if effect.uid == uid:
 			damage_effect_removed.emit(effect)
+			damage_effects_copy.erase(effect)
 			effect.queue_free()
+	damage_effects = damage_effects_copy
 
 # ----------------------------------------------------------------- #
 
 func on_effect_expired(effect: ActiveEffect) -> void:
+	if effect in active_effects:
+		active_effects.erase(effect)
+	elif effect in damage_effects:
+		damage_effects.erase(effect)
 	effect.queue_free()
